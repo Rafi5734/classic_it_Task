074e23e76db06789f64c6bda3e81fe7f09587549
Rafi5734 <kamrulhasun5734@gmail.com>
2023-12-09 12:13:00 +0600

    news section done

--- src/features/AllFeatures.jsx
+++ src/features/AllFeatures.jsx
@@ -2,8 +2,10 @@ import BannerSection2 from "./bannerSection2/BannerSection2";
 import BestProducts from "./bestProduct/BestProducts";
 import BestSellingProduct from "./bestSellingProduct/BestSellingProduct";
 import CategoryFeature from "./category_feature/CategoryFeature";
+import Customer from "./customer/Customer";
 import DealDiscount from "./deal&discount/DealDiscount";
 import DisplayFeature from "./display_feature/DisplayFeature";
+import News from "./news/News";
 import ProductBanner from "./productBanner/ProductBanner";
 
 const AllFeatures = () => {
@@ -16,6 +18,8 @@ const AllFeatures = () => {
       <ProductBanner />
       <BestSellingProduct />
       <BannerSection2 />
+      <Customer />
+      <News />
     </div>
   );
 };
---
+++ src/features/news/News.jsx
@@ -0,0 +1,214 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./news.css";
+
+const News = () => {
+  return (
+    <>
+      <Container className="mt-5 mb-5 work_container">
+        <h3 className="text-center">Our News</h3>
+        <p className="text-center" style={{ color: "#9296A3" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+
+        <div className="news_wrapper">
+          <Row>
+            <Col sm={12} md={6} lg={4}>
+              <div className="news_card">
+                <div className="news_card_img">
+                  <img
+                    src="https://img.freepik.com/free-photo/business-meeting-room-high-rise-office-building_105762-1717.jpg?w=826&t=st=1702091698~exp=1702092298~hmac=cf4558941212387193c54b2948f156f2ab587f0a37cf313e46fba1a60417b025"
+                    alt=""
+                  />
+                </div>
+                <h6> New Office</h6>
+                <h4> Our New Branch Office</h4>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Facere asperiores deserunt.
+                </p>
+                <a href="#">
+                  Read More{" "}
+                  <svg
+                    fill="#000000"
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 24 24"
+                    id="right-arrow"
+                    data-name="Flat Line"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon flat-line"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <line
+                        id="primary"
+                        x1="3"
+                        y1="12"
+                        x2="21"
+                        y2="12"
+                        style={{
+                          fill: "none",
+                          stroke: "#000000",
+                          strokeLinecap: "round",
+                          strokeLinejoin: "round",
+                          strokeWidth: "2",
+                        }}
+                      ></line>
+                      <polyline
+                        id="primary-2"
+                        data-name="primary"
+                        points="18 15 21 12 18 9"
+                        style={{
+                          fill: "none",
+                          stroke: "#000000",
+                          strokeLinecap: "round",
+                          strokeLinejoin: "round",
+                          strokeWidth: "2",
+                        }}
+                      ></polyline>
+                    </g>
+                  </svg>
+                </a>
+              </div>
+            </Col>
+            <Col sm={12} md={6} lg={4}>
+              <div className="news_card">
+                <div className="news_card_img">
+                  <img
+                    src="https://img.freepik.com/free-photo/business-meeting-room-high-rise-office-building_105762-1717.jpg?w=826&t=st=1702091698~exp=1702092298~hmac=cf4558941212387193c54b2948f156f2ab587f0a37cf313e46fba1a60417b025"
+                    alt=""
+                  />
+                </div>
+                <h6> New Office</h6>
+                <h4> Our New Branch Office</h4>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Facere asperiores deserunt.
+                </p>
+                <a href="#">
+                  Read More{" "}
+                  <svg
+                    fill="#000000"
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 24 24"
+                    id="right-arrow"
+                    data-name="Flat Line"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon flat-line"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <line
+                        id="primary"
+                        x1="3"
+                        y1="12"
+                        x2="21"
+                        y2="12"
+                        style={{
+                          fill: "none",
+                          stroke: "#000000",
+                          strokeLinecap: "round",
+                          strokeLinejoin: "round",
+                          strokeWidth: "2",
+                        }}
+                      ></line>
+                      <polyline
+                        id="primary-2"
+                        data-name="primary"
+                        points="18 15 21 12 18 9"
+                        style={{
+                          fill: "none",
+                          stroke: "#000000",
+                          strokeLinecap: "round",
+                          strokeLinejoin: "round",
+                          strokeWidth: "2",
+                        }}
+                      ></polyline>
+                    </g>
+                  </svg>
+                </a>
+              </div>
+            </Col>
+            <Col sm={12} md={6} lg={4}>
+              <div className="news_card">
+                <div className="news_card_img">
+                  <img
+                    src="https://img.freepik.com/free-photo/business-meeting-room-high-rise-office-building_105762-1717.jpg?w=826&t=st=1702091698~exp=1702092298~hmac=cf4558941212387193c54b2948f156f2ab587f0a37cf313e46fba1a60417b025"
+                    alt=""
+                  />
+                </div>
+                <h6> New Office</h6>
+                <h4> Our New Branch Office</h4>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Facere asperiores deserunt.
+                </p>
+                <a href="#">
+                  Read More{" "}
+                  <svg
+                    fill="#000000"
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 24 24"
+                    id="right-arrow"
+                    data-name="Flat Line"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon flat-line"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <line
+                        id="primary"
+                        x1="3"
+                        y1="12"
+                        x2="21"
+                        y2="12"
+                        style={{
+                          fill: "none",
+                          stroke: "#000000",
+                          strokeLinecap: "round",
+                          strokeLinejoin: "round",
+                          strokeWidth: "2",
+                        }}
+                      ></line>
+                      <polyline
+                        id="primary-2"
+                        data-name="primary"
+                        points="18 15 21 12 18 9"
+                        style={{
+                          fill: "none",
+                          stroke: "#000000",
+                          strokeLinecap: "round",
+                          strokeLinejoin: "round",
+                          strokeWidth: "2",
+                        }}
+                      ></polyline>
+                    </g>
+                  </svg>
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default News;
